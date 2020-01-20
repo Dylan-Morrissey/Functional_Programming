50107ff13809bbbbb13c3a2a233ba52359ef846c
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2020-01-20 10:38:44 +0000

    Completed Updated Lab 2

--- Labs/Lab_02.hs
+++ Labs/Lab_02.hs
@@ -92,3 +92,56 @@ tuple = (yourFirstName, yourAge)
 
 difference:: Double
 difference = pie - (22/7)
+
+----------------------------------
+-- add a type declaration
+-- to each of the named expressions
+i1:: Integer  -- I have done the first one for you
+i1 = 45
+
+i2 = "123"
+
+i3 = 45 <= i1
+
+i4 = 'c'
+
+i5 = ["abc","ok"]
+
+i6 = head i5
+
+i7 = tail "abc"  -- Recall a string is a shorthand for a list of Char
+
+i8 = (True,4.5)
+
+i9 = [i1,34]
+
+-------------------------------------------------
+-- For each named expression replace "undefined"
+-- with an expression with the same type as the declaration
+
+
+j1:: (String,Integer)
+j1 = (yourFirstName, yourAge)
+
+j2:: [Integer]
+j2 = [1..3]
+
+j3:: Char
+j3 = 'a'
+
+
+j4:: Double
+j4 = average
+
+
+j5:: (Integer,String,Integer,Char)
+j5 = (yourAge, yourFirstName, yourAge, j3)
+
+j6:: ([Char],(Bool,String))
+j6 = (['a'..'c'], (compareName, yourFirstName))
+
+j7:: [[Bool]]
+j7 = [[True, False]]
+
+j8:: [(String,Bool)]
+j8 = [(yourFirstName, compareName)]
