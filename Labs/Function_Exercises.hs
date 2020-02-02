ae38a122df7c5a0b461463dda5e8eb1a110ddd05
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2020-02-02 22:06:43 +0000

    Finished All Questions

--- Labs/Function_Exercises.hs
+++ Labs/Function_Exercises.hs
@@ -12,7 +12,7 @@ lucky a
     | otherwise = "Mmmm.... Can't really say ...."
 
 --Question 6
-first:: (a,b,c) -> a
+first :: (a,b,c) -> a
 first (a, _, _) = a
 second:: (a,b,c) -> b
 second (_, b, _) = b
@@ -20,3 +20,17 @@ third:: (a,b,c) -> c
 third (_,_,c) = c
 
 --Question 7 
+
+luhnDouble :: Int -> Int
+luhnDouble x = if (2 * x) > 9
+    then (2 * x) - 9
+    else 2 * x
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn x1 x2 x3 x4 = if 0 == sum[luhnDouble x1, x2, luhnDouble x3, x4] `mod` 10
+    then True
+    else False
+
+--Question 8
+luhnGetCheck:: Int -> Int -> Int -> Int
+luhnGetCheck a b c = 10 - sum[luhnDouble a, b, luhnDouble c] `mod` 10
--- Labs/List_Comprehensions.hs
+++ Labs/List_Comprehensions.hs
@@ -15,3 +15,20 @@ exc5 = [(x,1) | x <- [1..5]]
 
 --Exercise 6
 exc6 = [(x,x^2) | x <- [1..10]]
+
+--Exercise 7 
+f1 :: [(Int, Int)]
+f1 = [(x, y) | x <-[1..3], y<- [4..5]]
+f2 :: [(Int, Int)]
+f2 = [(x, y) | y<- [4..5], x <-[1..3]]
+f3 :: [(Int, Int)]
+f3 = [(y, x) | x <-[1..3], y<- [4..5]]
+
+--Exercise 8 
+isEven :: Integer -> Bool
+isEven n = (n `mod` 2 == 0)
+evenList = [2*n | n <- [2,4,7], isEven n, n>3]
+
+--Exercise 9
+doubleAll :: [Integer] -> [Integer]
+doubleAll [x] = [x..*2]
